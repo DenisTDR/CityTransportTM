4c1d39ffc1b8d6f894fba1106347cc903adb5dcd
DenisTDR <denis.tdr@gmail.com>
2016-09-06 21:04:12 +0300

    initial content for route stations

--- FE/ctt/app/pages/route-stations/route-stations.ts
+++ FE/ctt/app/pages/route-stations/route-stations.ts
@@ -1,3 +1,24 @@
+import {ApiService} from "../../services/api.service";
+import {MapPartial} from "../partials/map-partial/map-partial";
+import {RouteTypeMenu} from "../route-type-menu/route-type-menu";
+import {Component} from "@angular/core/esm";
+import {NavController, Platform} from 'ionic-angular';
 /**
  * Created by Aida on 9/5/2016.
  */
+
+@Component({
+  templateUrl: 'build/pages/routes-list/routes-list.html',
+  directives: [RouteTypeMenu, MapPartial],
+  providers: [ApiService]
+})
+export class RouteStationsPage {
+
+  constructor(private navCtrl: NavController, private apiService: ApiService) {
+    console.log("RouteStationsPage constructor");
+    // this.routeTypeMenu = RouteTypeMenu;
+  }
+  ngOnInit () {
+    console.log("ngoninit in RouteStationsPage");
+  }
+}
